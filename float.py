83fe0406ca44a908d06c4030332148d2701a245d
shyamharshit <shyamfreefire499@gmail.com>
2025-02-25 17:58:26 +0530

    integer

---
+++ float.py
@@ -0,0 +1,5 @@
+print=(234.34)
+#float(12.3,-839.087)
+# y=123.943
+# float=float(123.456)
+# print=(999 - float)
---
+++ int.py
@@ -0,0 +1,6 @@
+# print(123)
+#int(1,-23,456)
+x = -432
+int=int(-432)
+#int = -432 (value)
+print(123+ int)
